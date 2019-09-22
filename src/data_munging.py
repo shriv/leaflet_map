8baa252ac7234bdb3a5501d8031cc9928b0b0ddc
Shrividya Ravi <pentheseleia+bitbucket@gmail.com>
2019-09-22 19:28:45 +1200

    changing directory references and removing interactive hydrogen code

--- src/data_munging.py
+++ src/data_munging.py
@@ -1,8 +1,6 @@
 import pandas as pd
 
-fixit_df = pd.read_csv('~/Documents/remutaka-ct-analysis/qgis-mapping/Trapline Repairs - Sheet1.tsv', sep='\t')
-
-fixit_df.dtypes
+fixit_df = pd.read_csv('../data/Trapline Repairs - Sheet1.tsv', sep='\t')
 
 fixit_df['trap_code'] = (fixit_df['Trap Number']
                           .astype(str)
@@ -12,11 +10,8 @@ fixit_df['trap_code'] = (fixit_df['Trap Number']
 
 fixit_df['date'] = pd.to_datetime(fixit_df['Date Reported'], errors='coerce')
 
-loc_df = pd.read_csv('~/Documents/remutaka-ct-analysis/qgis-mapping/amalgamated_coordinates.csv', header=None)
-
+loc_df = pd.read_csv('../data/amalgamated_coordinates.csv', header=None)
 loc_df.columns = ['trap_code', 'easting', 'northing', 'lon', 'lat']
-
-loc_df.head()
 
 joined_df = pd.merge(fixit_df, loc_df, how = 'left')
 recent_df = joined_df.query('date > "2019-01-01"')
@@ -25,8 +20,6 @@ missing_trap_locs = (joined_df[joined_df['easting']
                     .isnull()]['Trap Number']
                     .drop_duplicates())
 
-joined_df.to_csv('~/Documents/remutaka-ct-analysis/qgis-mapping/joined_data.csv')
-
-recent_df.to_csv('~/Documents/remutaka-ct-analysis/qgis-mapping/joined_data_2019.csv')
-
+joined_df.to_csv('../data/joined_data.csv')
+recent_df.to_csv('../data/joined_data_2019.csv')
 fixit_df.shape
